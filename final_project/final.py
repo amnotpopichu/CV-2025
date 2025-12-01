49f1ea804954e6084aa178173ae3d222f95c1da1
Leo <153849409+amnotpopichu@users.noreply.github.com>
2025-11-30 19:04:32 -0800

    added comments

--- final_project/final.py
+++ final_project/final.py
@@ -4,17 +4,22 @@ import mss
 import time
 
 def setup():
+    #set up fps counter, pretty much justs define variables
     global new_frame_time, prev_frame_time
     new_frame_time = 0
     prev_frame_time = 0
 
 def fps(frame):
     global new_frame_time, prev_frame_time
+    #calculate fps by taking new time, then subtracting previous time, and doing 1/ all of that to get the fps
+    #this works because it captures a frame each iteration, and if we use this time delta every single "frame" then we can calculate fps
+    #tldr it works bc it counts time between frames, and from that you can get fps
     new_frame_time = time.time()
     fps = 1 / (new_frame_time - prev_frame_time)
+    #reset frame time
     prev_frame_time = new_frame_time
-    fps_text = f'fps: {str(int(fps))}'
     # Display FPS on the frame
+    fps_text = f'fps: {str(int(fps))}'
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame, fps_text, (10, 30), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
     return frame
@@ -38,21 +43,27 @@ def main():
     with mss.mss() as sct:
         monitor = sct.monitors[1]
         while True:
+            #get sct.grab as a frame
             sct_img = sct.grab(monitor)
             frame = sct_img
+            #convert to numpy array bc thats how opencv interprets images
             frame = np.array(frame)
+            
+            #if the frame doesnt exist, then exit
             if frame is None:
                 print("Can't read frame")
                 break
             
+            #process frame with line detection
             frame = line_detection(frame)
+            #fps counter
             frame = fps(frame)
+            #show frame
             cv2.imshow('lines in theory', frame)
             # Exit if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-
-        #js clean up when loop is broken (when user presses q to exit out of frame reading)
+        #cleanup
         cv2.destroyAllWindows()
 if __name__ == "__main__":
     setup()
